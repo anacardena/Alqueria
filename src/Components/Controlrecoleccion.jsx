2a6df378602e7a07b7d11bf4e69966231e212a97
AnaCardenas <abcardenasb@gmail.com>
2021-07-22 09:12:32 -0500

    enlaces terminados

--- src/Components/Controlrecoleccion.jsx
+++ src/Components/Controlrecoleccion.jsx
@@ -3,16 +3,19 @@ import '../utils/css/controlrecoleccion.css'
 import '../utils/css/estilosglobales.css'
 import {Link} from "react-router-dom"
 import logo from "../utils/images/logoalquería.jpg"
+import atras from "../utils/images/icons8-volver-50.png"
+import inicio from "../utils/images/icons8-casa-24.png"
+
 function Controlrecoleccion() {
 	return (
 		<div id="padre_control"> 
       
       <div id="banner" style={{backgroundImage:`url(${logo})`}}> </div>
     <Link to="/Recoleccion" class="btn">
-             <span>atrás</span>
+             <span><img src={atras} /></span>
              </Link>
              <Link to="/" class="btn">
-             <span>inicio</span>
+             <span><img src={inicio} /></span>
              </Link>
       <h1> la vaca lola </h1>
       <div id ="parteinferior">
--- src/Components/Recoleccion.jsx
+++ src/Components/Recoleccion.jsx
@@ -3,19 +3,29 @@ import '../utils/css/recoleccion.css'
 import '../utils/css/estilosglobales.css'
 import logo from '../utils/images/logoalquería.jpg'
 import {Link} from "react-router-dom"
+import atras from "../utils/images/icons8-volver-50.png"
+import buscar from "../utils/images/icons8-búsqueda-24.png"
 function Recoleccion () {
     return (
     	<div id="contenedor_padre">
+
     	 <div id="banner" style={{backgroundImage:`url(${logo})`}}> </div>
-    	 <h1> aquí se tiene el listado de las gransjas y se registrarán las leches </h1>
-        <div id="boton">
+    	  <div id="boton">
               <Link to="/" class="btn">
-             <span>atrás</span>
+             <span><img src={atras} /></span>
              </Link>
+            
+	    </div>
+    	 <h1> aquí se tiene el listado de las gransjas y se registrarán las leches </h1>
+    	 <form>
+    	  <input type="text" id="searchterm" placeholder="digite el nombre de la granja" />
+          <button type="button" id="search"><img src={buscar} /></button>
+    	 </form>
+        <div className="granjas">
              <Link to="/Controlrecoleccion" class="btn">
              <span>Controlrecoleccion</span>
              </Link>
-	    </div>
+             </div>
 	    </div>
     )
 
